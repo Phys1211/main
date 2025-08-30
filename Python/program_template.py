f9bca4b04948c5576c4d9c8bad790f1088116733
tkelley3 <th.kelley@northeastern.edu>
2025-08-30 15:40:41 -0400

    Aug 30 Update

---
+++ Python/program_template.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on ...
+
+@author: ...
+Description: ...
+    
+"""
+
+# Define functions here
+
+def main():
+    #Program body
+    print('Some output')
+    return 0
+    
+#The code below runs whatever is in main()
+if __name__ == '__main__':
+    main()
