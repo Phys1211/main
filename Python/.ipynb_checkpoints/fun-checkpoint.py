f9497efbddc7a01a03604a0248b3a7cd813a7d34
tkelley3 <th.kelley@northeastern.edu>
2025-10-07 14:47:35 -0400

    Oct 7 Update

---
+++ Python/.ipynb_checkpoints/fun-checkpoint.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Tom
+Documentation here will appear when running
+import funcpass as fp
+fp.__dir__
+"""
+
+import math as mh
+
+def rising1D(v0,g=9.8):
+    '''
+    Parameters
+    ----------
+    v0 : float
+        initial velocity of object fired up.
+    g : float
+        acceleration due to gravity. The default is 9.8.
+
+    Returns
+    -------
+    h : float
+        Maximum height of object.
+    t : float
+        time in the air.
+
+    '''
+    h = v0**2/(2*g)
+    t = v0/g
+    return h,t
+
+def falling1D(h,v0=0):
+    # returns the time and velocity of an object falling from h and at an initial velocity v0
+    # convention v0>0 is downward velocity
+    g = 9.8
+    #solution to quadratic equation
+    time = -v0/g + mh.sqrt(v0**2/g**2+2*h/g)
+    velo = mh.sqrt(v0**2+2*g*h)
+    return time,velo
+
+def projectile(v0, angle, h):
+    height, timeup = rising1D(v0)
+    timedown, vf = falling1D(height)
+    time = timeup + timedown
+    rang = v0*time
+    return vf, rang, time, height
+
+if __name__ == '__main__':
+    [height,timeup] = rising1D(80)
+    [timedown, vf] = falling1D(height)
+    print(timedown)
+
+
+
+
